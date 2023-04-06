b91f6b18f0f45c1c01b5e9e78c55263ce82f6a6b
atreus.engel <atreus.engelniederhammer@seitwerk.de>
2023-04-06 15:11:01 +0200

    code: added working !Dienst funktion

--- jsBot/index.js
+++ jsBot/index.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
-const {Client, MessageActionRow, MessageButton, GatewayIntentBits} = require('discord.js');
+const {Client, MessageActionRow, MessageButton, message, GatewayIntentBits, Embed, command, EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -33,10 +37,57 @@ client.on('ready', () => {
     console.log(`Bot ist eingeloggt als ${client.user.tag}`);
 });
 
-client.on('messageCreate', (message) => {
-    if (message.content.toLowerCase() === '#ping') { // Vergleicht den Inhalt der Nachricht (nicht case-sensitive) mit 'oing'
-        message.reply('Pong'); // Antwortet mit 'Pong' auf die Nachricht
+client.on('messageCreate', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
     }
 });
+
+
+client.on('messageCreate', message => {
+    if (message.content === '!Dienst') {
+        const {ActionRowBuilder, ButtonBuilder, ButtonStyle} = require('discord.js');
+        const button1 = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('button')
+                    .setLabel('Dienst Starten')
+                    .setStyle(ButtonStyle.Success),
+            );
+        const button2 = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('button')
+                    .setLabel('Dienst beendet')
+                    .setStyle(ButtonStyle.Danger),
+            );
+        const embed = new EmbedBuilder()
+            .setColor("Green")
+            .setDescription(`Dienst beginnen`)
+
+        const embed2 = new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(`${message.author.tag} ist jetzt im Dienst`)
+
+        message.channel.send({embeds: [embed], components: [button1]});
+
+        const collector = message.channel.createMessageComponentCollector();
+
+        collector.on('collect', async i => {
+            if (i.customId === 'button') {
+                if (i.component.label === 'Dienst beendet') {
+                    await i.deferUpdate();
+                    await i.editReply({ content: 'Dienst beendet.', components: [] });
+                    collector.stop();
+                    i.message.delete();
+                } else {
+                    await i.deferUpdate();
+                    await i.editReply({ content: 'Dienst gestartet.', embeds: [embed2], components: [button2] });
+                }
+            }
+        });
+    }
+});
+
 
 client.login('MTA5MjM4MTg1NzU5MDU1NDY4NQ.G3P2PQ.mV6owLWJcbtyzKiA3BEbC3FAKQ50a_RHIKV4G8');
